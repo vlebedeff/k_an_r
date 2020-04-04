d88db1d77bf27cf92a5c938172d3a0a3efa546e7
Vlad Lebedev <vladlebedeff@gmail.com>
2020-04-04 10:39:04 +0300

    Add astyle formatter

--- strrindex.c
+++ strrindex.c
@@ -1,20 +1,24 @@
 #include <string.h>
 #include <stdio.h>
 
-int strrindex(char src[], char pattern[]) {
+int strrindex(char src[], char pattern[])
+{
     int src_len = strlen(src);
     int pattern_len = strlen(pattern);
 
-    if (src_len == 0 && pattern_len == 0)
+    if (src_len == 0 && pattern_len == 0) {
         return 0;
+    }
 
     int i, j, k;
 
     for (i = src_len - pattern_len - 1; i >= 0; i--) {
         for(j = i, k = 0; pattern[k] != '\0' && src[j] == pattern[k]; j++, k++);
 
-        if (k > 0 && pattern[k] == '\0')
+        if (k > 0 && pattern[k] == '\0') {
             return i;
+        }
     }
+
     return -1;
 }
--- strrindex_test.c
+++ strrindex_test.c
@@ -4,35 +4,43 @@
 
 #include "strrindex.h"
 
-void test_strrindex_example_1() {
+void test_strrindex_example_1()
+{
     assert(strrindex("hello", "e") == 1);
 }
 
-void test_strrindex_example_2() {
+void test_strrindex_example_2()
+{
     assert(strrindex("hello", "l") == 3);
 }
 
-void test_strrindex_example_3() {
+void test_strrindex_example_3()
+{
     assert(strrindex("hello", "a") == -1);
 }
 
-void test_strrindex_example_4() {
+void test_strrindex_example_4()
+{
     assert(strrindex("ACTGGTCAGGTCA", "GG") == 8);
 }
 
-void test_strrindex_example_5() {
+void test_strrindex_example_5()
+{
     assert(strrindex("", "GG") == -1);
 }
 
-void test_strrindex_example_6() {
+void test_strrindex_example_6()
+{
     assert(strrindex("ACTGGTCAGGTCA", "") == -1);
 }
 
-void test_strrindex_example_7() {
+void test_strrindex_example_7()
+{
     assert(strrindex("", "") == 0);
 }
 
-int main() {
+int main()
+{
     test_strrindex_example_1();
     test_strrindex_example_2();
     test_strrindex_example_3();
